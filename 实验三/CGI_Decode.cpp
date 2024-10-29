17dcb7aad9bb34c09dd002e538fa996f2a541906
xfuyb <107780400+xfuyb@users.noreply.github.com>
2024-10-29 14:45:41 +0800

    Add files via upload

---
+++ 实验三/CGI_Decode.cpp
@@ -0,0 +1,94 @@
+// CGI_Decode.cpp : Defines the entry point for the console application.
+//
+
+#include <iostream>
+#include <string.h>
+
+using namespace std;
+
+/*Get the hex value for a given character*/
+int getHexValue(char c)
+{
+	int hValue = -1;
+	if (c >= '0' && c <= '9')
+	{
+		hValue = c - '0';
+	}
+	else if (c >= 'A' && c <= 'F')
+	{
+		hValue = c - 'A' + 10;
+	}
+	else if (c >= 'a' && c <= 'f')
+	{
+		hValue = c - 'a' + 10;
+	}
+	return hValue;
+}
+
+/**    Translate a string from the CGI encoding to plain ascii text.
+ *     '+' becomes space, %xx becomes byte with hex value xx,
+ *    other alphanumeric characters map to themselves.
+ *    Returns 0 for success, positive for erroneous input
+ *         1 = bad hexadecimal digit
+ */
+int decode(const char *encoded, char *decoded)
+{
+	const char *eptr = encoded;
+	char *dptr = decoded;
+	int ok = 0;
+	while (*eptr)
+	{
+		char c;
+		c = *eptr;
+		if (c == '+')
+		{ /* Case 1: '+' maps to blank */
+			*dptr = ' ';
+		}
+		else if (c == '%')
+		{ /* Case 2: '%xx' is hex for character xx */
+			int digit_high = getHexValue(*(++eptr));
+			int digit_low = getHexValue(*(++eptr));
+			if (digit_high == -1 || digit_low == -1)
+			{
+				/* *dptr='?'; */
+				ok = 1; /* Bad return code */
+			}
+			else
+			{
+				*dptr = 16 * digit_high + digit_low;
+			}
+		}
+		else
+		{ /* Case 3: All other characters map to themselves */
+			*dptr = *eptr;
+		}
+		++dptr;
+		++eptr;
+	}
+	*dptr = '\0'; /* Null terminator for string */
+	return ok;
+}
+
+int main()
+{
+	int ok;
+	const char *input = "https://example.com/search?q=Hello+World%20Test%25%2F%4aEnd"; /*This is a test case*/
+	char *output = (char *)malloc(strlen(input) + 1);
+
+	ok = decode(input, output);
+
+	if (ok == 0)
+	{
+		cout << "Decode success!	output:" << output << endl;
+	}
+	else if (ok > 0)
+	{
+		cout << "erroneous input" << endl;
+		if (ok == 1)
+		{
+			cout << "bad hexadecimal digit" << endl;
+		}
+	}
+
+	return 0;
+}
